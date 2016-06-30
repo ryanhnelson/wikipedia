f6c26b1dda17d000a40403f6d284fa388ecebae9
Ryan Nelson <ryanhnelson@gmail.com>
2016-06-30 11:14:04 +0200

    rn

--- human-development-index/01-hdi-cleaning-and-importing.R
+++ human-development-index/01-hdi-cleaning-and-importing.R
@@ -1,4 +1,4 @@
- 
+ # Cleaning and importing data for project
 
 library(xlsx)
 library(tidyr)
@@ -19,12 +19,13 @@ pop <- read.xlsx("WPP2015_POP_F01_1_TOTAL_POPULATION_BOTH_SEXES.XLS", 1,
                  startRow = 17, encoding = "UTF-8")
 pop <- dplyr::select(pop, -Index, -Variant, -Notes, -Country.code)
 pop <- rename(pop, Country = Major.area..region..country.or.area..)
-pop <- pop[-c(1:14, 35, 45, 59, 77, 78, 88, 94,
-              104, 135, 161, 178, 188, 189, 216, 
-              240, 246),]
+pop <- pop[-c(1:14, 35, 45, 53, 59, 77, 78, 88, 89, 94,
+              104, 116, 135, 136, 147, 161, 178, 188, 189, 216, 
+              225, 240, 246, 247, 250, 256, 264),]
 pop <- tidyr::gather(pop, "Year", "Population", 2:67)
 pop <- dplyr::arrange(pop, Country)
 pop$Year <- substr(pop$Year, 2, 5)
+pop$Population <- pop$Population * 1000 # Was denominated in thousands
 
-str(pop)
-utils::View(pop)
+ # str(pop)
+ # utils::View(pop)
